0b4440214455685b38d8490d1b6b900c876bbbc2
Vinicius Leal <viniciusleal.s@gmail.com>
2025-05-08 14:41:08 -0300

    fix: update logo source URL and remove unused logo file

--- studio/components/Logo.tsx
+++ studio/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VinicoLogo() {
   return (
     <img 
-    src='./components/logo.png'
+    src='https://l3pden50fc.ufs.sh/f/qVxhWi9olDGtbhbcXCAgqS7DnVdjakXxi8FyRKutmBTf5bHh'
       alt="Sane Kit Logo" 
         style={{ width: '20px', height: 'auto' }}
     />
